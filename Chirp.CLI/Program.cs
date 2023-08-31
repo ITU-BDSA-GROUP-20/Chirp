475d77853d285612e6b4e00e47434563001718d3
Mikkel <mbek@itu.dk>
2023-08-31 15:50:40 +0200

    Chirp 0.1 - Read from .csv file
    Co-Authored-By: Mads Dalgaard Andersen <92082406+Davdu@users.noreply.github.com>
    Co-Authored-By: Casper Holten <69851314+casperh123@users.noreply.github.com>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+if (args.Length != 2)
+{
+    Console.WriteLine("Command usage: <command> <action>");
+    return;
+}
+
+if (args[0].Equals("read"))
+{
+    int noOfLines = int.Parse(args[1]);
+
+    using (StreamReader reader = new StreamReader("C:\\ITU\\Semester 3\\Software Arch\\Chirp\\Chirp.CLI\\chirp_cli_db.csv")) 
+    {
+        while (!reader.EndOfStream)
+        {
+            string author;
+            string message;
+            int timeStamp;
+            Regex regex = new Regex("[,\"][\",]");
+            string line = reader.ReadLine();
+            string[] splitLine = regex.Split(line);
+            
+            author = splitLine[0];
+            message = splitLine[1];
+            timeStamp = int.Parse(splitLine[2]);
+            DateTimeOffset time = DateTimeOffset.FromUnixTimeSeconds(timeStamp);
+            
+            Console.WriteLine($"{author} @ {time}: {message} ");
+        }
+    }
+}
+
