a3cf2cd7f7cb30904866159299bf2223e60431c8
Oliver Laursen <olau@itu.dk>
2023-09-05 10:24:47 +0200

    Add cheep functionality
    Added an "else-if" statement that uses "cheep" as triggerword, and then takes a message which it writes to the CSV file.
    We've done this inorder to write new cheeps to our CSV file, expanding the interactivity of our application.
    
    Co-Authored-By: Mads Dalgaard Andersen <92082406+Davdu@users.noreply.github.com>
    Co-Authored-By: Mikkel Kristensen <110497736+MikkelBKristensen@users.noreply.github.com>
    Co-Authored-By: Qezert <114524815+Qezert@users.noreply.github.com>
    Co-Authored-By: Casper Holten <69851314+casperh123@users.noreply.github.com>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -5,12 +5,12 @@ if (args.Length != 2)
     Console.WriteLine("Command usage: <command> <action>");
     return;
 }
-
+var csvFilePath = "chirp_cli_db.csv";
 if (args[0].Equals("read"))
 {
     int noOfLines = int.Parse(args[1]);
 
-    using (StreamReader reader = new StreamReader("C:\\ITU\\Semester 3\\Software Arch\\Chirp\\Chirp.CLI\\chirp_cli_db.csv")) 
+    using (StreamReader reader = new StreamReader(csvFilePath)) 
     {
         while (!reader.EndOfStream)
         {
@@ -18,7 +18,7 @@ if (args[0].Equals("read"))
             string message;
             int timeStamp;
             Regex regex = new Regex("[,\"][\",]");
-            string line = reader.ReadLine();
+            string line = reader.ReadLine()!;
             string[] splitLine = regex.Split(line);
             
             author = splitLine[0];
@@ -29,5 +29,21 @@ if (args[0].Equals("read"))
             Console.WriteLine($"{author} @ {time}: {message}");
         }
     }
+} else if(args[0] == "cheep" )
+{
+    string message = args[1];
+    //Src: "user142162" https://stackoverflow.com/questions/1240373/how-do-i-get-the-current-username-in-net-using-c
+    string author = Environment.UserName; //Gets the current users username. 
+   
+    //Src: "Andrew" https://stackoverflow.com/questions/17632584/how-to-get-the-unix-timestamp-in-c-sharp 
+    long unixTimeStamp = (long)DateTime.Now.Subtract(DateTime.UnixEpoch).TotalSeconds;
+    
+    string cheep = $"{author},\"{message}\",{unixTimeStamp}";
+
+    //Write to CSV file
+
+    using StreamWriter sw = File.AppendText(csvFilePath);
+    sw.WriteLine(cheep);
+
 }
 
