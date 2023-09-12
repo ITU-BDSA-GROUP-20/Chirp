0076a71e2e930632c95c9d85c633a527cd9467cc
Davdu <madsd@itu.dk>
2023-09-12 11:10:39 +0200

    Refactorisation of main method (In progress)
    Extracted methods from the main method to improve readability and extensibility
    
    Co-Authored-By: Oliver Laursen <olau@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,28 +1,56 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.ComponentModel;
 using Chirp.CLI;
 using SimpleDB;
 
-string csvFilePath = "chirp_cli_db.csv";
-IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(csvFilePath);
+public class Program
+{
+    static string csvFilePath = "chirp_cli_db.csv";
+    private static IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(csvFilePath);
+    
+    public static void Main(string[] args)
+    {
+        try
+        {
+            ReadCommand(args);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("Read command usage: read <integer>");
+        }
+        catch (Exception e)
+        {
+            // Temporary
+            Console.WriteLine("Command usage: <command> <action>");
+            return;
+        }
+    }
+
+    private static void ReadCommand(string[] args)
+    {
+        switch (args[0])
+        {
+            case "read":
+                ReadCheeps(int.Parse(args[1]));
+                break;
+            case "cheep":
+                WriteCheep(args[1]);
+                break;
+            default:
+                throw new Exception();
+        }
+    }
+
+    private static void ReadCheeps(int noOfLines)
+    {
+        UserInterface.PrintCheeps(database.Read(noOfLines));
+    }
+
+    private static void WriteCheep(string message)
+    {
+        //database.Store(new Cheep(message))
+    }
 
 
-if (args.Length != 2)
-{
-    Console.WriteLine("Command usage: <command> <action>");
-    return;
-}
-if (args[0].Equals("read"))
-{
-    int noOfLines = int.Parse(args[1]);
-    UserInterface.PrintCheeps(database.Read(noOfLines));
-    
-} else if(args[0] == "cheep" )
-{
-    string message = args[1];
-    
-    //Converting from timestamp to unixtimestamp... 
-    
-    //database.Store(new Cheep(message));
 }
 
 
