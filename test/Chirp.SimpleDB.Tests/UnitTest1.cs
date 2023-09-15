fa83743cf77f53167db28f78e3c72d72ee67d53d
Oliver Laursen <olau@itu.dk>
2023-09-15 10:08:09 +0200

    Setup .sln correctly
    Updated old filepaths and added the csproj files from the test projects to the .sln file.

--- test/Chirp.SimpleDB.Tests/UnitTest1.cs
+++ test/Chirp.SimpleDB.Tests/UnitTest1.cs
@@ -2,8 +2,18 @@ namespace Chirp.SimpleDB.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    //Make integrationtest of SimpleDB
+    
+    
+    
+    
+    //Testing that Cheeped cheeps get stored in the csv file. 
+    
+    [Theory]
+    [InlineData("Hello")]
+    [InlineData("Hi, my name is TestPerson.")]
+    public void Ad(string message)
     {
+        
     }
 }
