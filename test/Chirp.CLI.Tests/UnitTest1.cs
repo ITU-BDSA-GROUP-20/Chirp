dad004e176bf4ac7e8405bd24e47bd08e3c0925f
Mikkel <mbek@itu.dk>
2023-09-19 12:29:55 +0200

    Fix Project references
    Fix project references so that the src folder can be called from the test folder.
    
    Co-Authored-By: Oliver Laursen <olau@itu.dk>
    Co-Authored-By: Magnus Kristensen <bekr@itu.dk>

--- test/Chirp.CLI.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Tests/UnitTest1.cs
@@ -3,7 +3,15 @@ namespace Chirp.CLI.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestCheepToString()
     {
+        // Arrange
+        Cheep cheep = new Cheep("test", "test", 1695108998);
+
+        // Act
+        var output = cheep.ToString();
+
+        // Assert
+        Assert.Equal("test @ 19/09/2023 09:36:38 test", output);
     }
 }
