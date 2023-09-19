57fc543e1ec856bd822e9205d51b3548f354c267
Mikkel <mbek@itu.dk>
2023-09-19 10:56:46 +0200

    Fix bug to be able to reference projects in /src
    Co-Authored-By: Oliver Laursen <olau@itu.dk>
    Co-Authored-By: Magnus Kristensen <bekr@itu.dk>

--- test/Chirp.CLI.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Tests/UnitTest1.cs
@@ -1,9 +1,21 @@
-namespace Chirp.CLI.Tests;
+using SimpleDB;
 
+namespace Chirp.CLI.Tests;
+using System;
+
+using SimpleDB;
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        // Arrange
+        var expected = 1;
+
+        // Act
+        var actual = 1;
+
+        // Assert
+        Assert.Equal(expected, actual);
     }
 }
