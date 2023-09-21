117018fac69d4a989c12c7c0952464cf8a55b945
Casper Holten <casho@itu.dk>
2023-09-21 10:59:52 +0200

    Fix datetime test
    DateTime function calculates the correct system specific time
    
    Co-Authored-By: Mads Dalgaard Andersen <madsd@itu.dk>

--- test/Chirp.CLI.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Tests/UnitTest1.cs
@@ -12,6 +12,9 @@ public class UnitTest1
         var output = cheep.ToString();
 
         // Assert
-        Assert.Equal("test @ 19-09-2023 09:36:38 test", output);
+        Assert.Equal("test" + " @ " +
+                     DateTimeOffset.FromUnixTimeSeconds(1695108998).LocalDateTime + " "
+                     + "test", output);
+        
     }
 }
