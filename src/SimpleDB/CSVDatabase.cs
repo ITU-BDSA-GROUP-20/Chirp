369722dec29272fc99a17b93b4a6818f1d831e9a
Casper Holten <casho@itu.dk>
2023-09-21 15:59:07 +0200

    Revert "Web hosting NOT WORKING"
    This reverts commit 1e8542b3adccfcf289450347c66138a043f8309b.

--- src/SimpleDB/CSVDatabase.cs
+++ src/SimpleDB/CSVDatabase.cs
@@ -6,7 +6,7 @@ namespace SimpleDB;
 
 public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 {
-    private static string filePath = @"../data/cheeps.csv";
+    private static string filePath = @"../../data/chirp_cli_db.csv";
     List<T> cheepCollection;
     private static CSVDatabase<T> instance;
 
