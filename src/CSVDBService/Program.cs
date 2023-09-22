ecdf01c16996c86fdd1b3bc2747589f74d8874c7
Mads D Andersen <madsd@itu.dk>
2023-09-22 14:37:44 +0200

    Deployment debugging test
    Currently the service works as intended on local hosts.
    However, when deployed to azure, all functionality fails

--- src/CSVDBService/Program.cs
+++ src/CSVDBService/Program.cs
@@ -6,9 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 IDatabaseRepository<Cheep> database = CSVDatabase<Cheep>.Instance;
-app.MapGet("/cheeps", () => database.Read(10));
+app.MapGet("/cheeps", () => new Cheep("Mads", "Hello World!", 5485489798799));//database.Read(10));
 
 app.Run();
-//
+
 
 public record Cheep(string Author, string Message, long Timestamp);
