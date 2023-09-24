99bfa374c52a688143070cc94c657c37899a3753
Casper Holten <casho@itu.dk>
2023-09-24 21:43:01 +0200

    Update cheeps endpoint
    Enable sending of cheeps

--- src/CSVDBService/Program.cs
+++ src/CSVDBService/Program.cs
@@ -20,6 +20,8 @@ app.MapGet("/cheeps", () =>
     return null;
 });
 
+app.MapGet("/cheep", (Cheep cheep) => { database.Store(cheep); });
+
 app.Map("/", () => "You have reached Group 20's Chirp Server");
 
 app.Run();
