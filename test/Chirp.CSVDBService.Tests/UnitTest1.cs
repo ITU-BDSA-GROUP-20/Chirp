a33811d6c5140b4db48898a259ed94af90ae65b4
Oliver Laursen <olau@itu.dk>
2023-09-26 14:04:50 +0200

    Testcase for CSVDBService

--- test/Chirp.CSVDBService.Tests/UnitTest1.cs
+++ test/Chirp.CSVDBService.Tests/UnitTest1.cs
@@ -8,13 +8,51 @@ public class UnitTest1
     
     private IDatabaseRepository<Cheep> database = CSVDatabase<Cheep>.Instance;
     
-    [Fact]
-    public void Test1()
+    
+    public void SetUp()
     {
         database.SetFilePath("./data/cheep-test");
         
-        database.Store(new Cheep("Hej", "hej", 12355431));
-            
+    }
+    public void TearDown()
+    {
         File.Delete("./data/cheep-test");
     }
+    
+    //Methods to test:
+    //read - 
+    //store - 
+
+
+
+    [Fact]
+    public void Read_ShouldReadFirstCheepAuthorAndMessage()
+    {
+        SetUp();
+        database.Store(new Cheep("TestAuthor", "Test, message", 12355431));
+
+        List<Cheep> output = (List<Cheep>)database.Read(1);
+        Assert.StartsWith("TestAuthor", output[0].Author);
+        Assert.StartsWith("Test", output[0].Message);
+        TearDown();
+    }
+
+    // [Fact]
+    // public void Store_ShouldStoreExactNumberOfCheeps(Cheep cheep1, Cheep cheep2)
+    // {
+    //     SetUp();
+    //     database.Store(new Cheep("Auth1", "Message1", 1695728248));
+    //     database.Store(new Cheep("Auth2", "Message2", 1695728260));
+
+    //     List<Cheep> output =new(database.Read(3));
+        
+    //     Assert.Equal(2, output.Count);
+    //     TearDown();
+    // }
+
+    
+
+
+    
+    
 }
