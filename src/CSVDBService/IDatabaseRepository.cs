bd29d67b19ee6ccaa4a91470ceb7259526964c14
Casper Holten <casho@itu.dk>
2023-09-26 11:24:50 +0200

    Setup test template
    Refactor IDatabaseRepository to include setPath to enable decoupled testing.
    
    Co-Authored-By: Mads Dalgaard Andersen <madsd@itu.dk>
    Co-Authored-By: Mikkel Kristensen <mbek@itu.dk>
    Co-Authored-By: Oliver Laursen <olau@itu.dk>
    Co-Authored-By: Magnus Kristensen <bekr@itu.dk>

--- src/CSVDBService/IDatabaseRepository.cs
+++ src/CSVDBService/IDatabaseRepository.cs
@@ -3,4 +3,5 @@ public interface IDatabaseRepository<T>
 {
     public IEnumerable<T> Read(int limit);
     public void Store(T record);
+    public void SetFilePath(string filePath)
 }
--- test/Chirp.CSVDBService.Tests/UnitTest1.cs
+++ test/Chirp.CSVDBService.Tests/UnitTest1.cs
@@ -1,9 +1,20 @@
+using CSVDBService;
+
+
 namespace Chirp.CSVDBService.Tests;
 
 public class UnitTest1
 {
+    
+    private IDatabaseRepository<Cheep> database = CSVDatabase<Cheep>.Instance;
+    
     [Fact]
     public void Test1()
     {
+        database.SetFilePath("./data/cheep-test");
+        
+        database.Store(new Cheep("Hej", "hej", 12355431));
+            
+        File.Delete("./data/cheep-test");
     }
 }
