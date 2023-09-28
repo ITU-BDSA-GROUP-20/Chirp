724bef6178031e176c3cf2724a10295743ff5d2d
Mikkel Kristensen <mbek@itu.dk>
2023-09-28 15:07:29 +0200

    Add Unit test from .CLI program

--- test/Chirp.Razor.Tests/UnitTest1.cs
+++ test/Chirp.Razor.Tests/UnitTest1.cs
@@ -3,7 +3,17 @@ namespace Chirp.Razor.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestCheepToString()
     {
+        // Arrange
+        var cheep = new Cheep("test", "test", 1695108998);
+
+        // Act
+        var output = cheep.ToString();
+
+        // Assert
+        Assert.Equal("test" + " @ " +
+                     DateTimeOffset.FromUnixTimeSeconds(1695108998).LocalDateTime + " "
+                     + "test", output);
     }
 }
