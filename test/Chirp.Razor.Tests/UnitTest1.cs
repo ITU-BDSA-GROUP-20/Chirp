0879bc9f763a1618b55125753bfe4118fb157b22
Mikkel <mbek@itu.dk>
2023-09-30 11:25:54 +0200

    Remove incompatible unittest

--- test/Chirp.Razor.Tests/UnitTest1.cs
+++ test/Chirp.Razor.Tests/UnitTest1.cs
@@ -2,18 +2,4 @@ namespace Chirp.Razor.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void TestCheepToString()
-    {
-        // Arrange
-        var cheep = new Cheep("test", "test", 1695108998);
-
-        // Act
-        var output = cheep.ToString();
-
-        // Assert
-        Assert.Equal("test" + " @ " +
-                     DateTimeOffset.FromUnixTimeSeconds(1695108998).LocalDateTime + " "
-                     + "test", output);
-    }
 }
