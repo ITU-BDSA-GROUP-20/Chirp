b63b0df711f6fd5c702e071702a73be72e2be500
Mikkel <mbek@itu.dk>
2023-10-02 01:16:21 +0200

    Try to make integration tests run

--- test/Chirp.Razor.Tests/IntegrationTest.cs
+++ test/Chirp.Razor.Tests/IntegrationTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 
@@ -6,5 +7,37 @@ namespace Chirp.Razor.Tests;
 
 public class IntegrationTest : IClassFixture<WebApplicationFactory<Program>>
 { 
+    private readonly WebApplicationFactory<Program> _fixture;
+    private readonly HttpClient _client;
+
+    public IntegrationTest(WebApplicationFactory<Program> fixture)
+    {
+        _fixture = fixture;
+        _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
+    }
+
+    [Fact]
+    public async void CanSeePublicTimeline() 
+    {
+        var response = await _client.GetAsync("/public");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains("Public Timeline", content);
+    }
+
+    [Theory]
+    [InlineData("Helge")]
+    [InlineData("Rasmus")]
+    public async void CanSeeUserTimeline(string author)
+    {
+        var response = await _client.GetAsync($"/user/{author}");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains($"{author}'s Timeline", content);
+    }
     
 }
