960bc9114aeb74a7f48550aff43e42b30e6984e2
Oliver Laursen <olau@itu.dk>
2023-10-02 22:25:47 +0200

    Implement SQLite in CheepService
    Add reference from Chirp.SQLite to Chirp.Razor, and refactor CheepService to utilize the SQLite database.
    
    Co-Authored-By: Mads Dalgaard Andersen <madsd@itu.dk>
    Co-Authored-By: Magnus Kristensen <bekr@itu.dk>
    Co-Authored-By: Mikkel Kristensen <mbek@itu.dk>
    Co-Authored-By: Casper Holten <casho@itu.dk>

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -1,3 +1,5 @@
+using Chirp.SQLite;
+using Microsoft.VisualBasic;
 
 public record CheepViewModel(string Author, string Message, string Timestamp);
 
@@ -10,22 +12,40 @@ public interface ICheepService
 
 public class CheepService : ICheepService
 {
-    // These would normally be loaded from a database for example
-    private static readonly List<CheepViewModel> _cheeps = new()
-        {
-            new CheepViewModel("Helge", "Hello, BDSA students!", UnixTimeStampToDateTimeString(1690892208)),
-            new CheepViewModel("Rasmus", "Hej, velkommen til kurset.", UnixTimeStampToDateTimeString(1690895308)),
-        };
+    DBFacade DB = new();
+
 
     public List<CheepViewModel> GetCheeps()
     {
+        //query ran:
+        //SELECT user.username, message.text, message.pub_date FROM message JOIN user on message.author_id = user.user_id ORDER by message.pub_date desc
+        List<Object> unrefinedList = DB.GetAllMessages();
+
+        return FormatCheeps(unrefinedList);
+        
+    }
+    private List<CheepViewModel> FormatCheeps(List<Object> unformattedList){
+        var _cheeps = new List<CheepViewModel>();
+        for(int i = 0; i<unformattedList.Count-3; i+=3){
+
+            _cheeps.Add(new CheepViewModel(
+                //Needs to conform to order of how we retrieve data from the query
+                unformattedList[i].ToString()!,
+                unformattedList[i+1].ToString()!,
+                UnixTimeStampToDateTimeString(Convert.ToDouble(unformattedList[i+2]))));
+    
+        }
         return _cheeps;
     }
 
     public List<CheepViewModel> GetCheepsFromAuthor(string author)
     {
+        //query ran:
+        //SELECT user.username, message.text, message.pub_date FROM message JOIN user ON message.author_id = user.user_id WHERE user.username = @Author ORDER by message.pub_date desc
+       
+        List<Object> unformattedList = DB.getAuthorsMessages(author);
         // filter by the provided author name
-        return _cheeps.Where(x => x.Author == author).ToList();
+        return FormatCheeps(unformattedList);
     }
 
     private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
--- src/Chirp.SQLite/DBFacade.cs
+++ src/Chirp.SQLite/DBFacade.cs
@@ -1,3 +1,4 @@
+namespace Chirp.SQLite;
 
 using System.Data;
 using System.Reflection;
@@ -25,9 +26,7 @@ public class DBFacade{
 
     private List<Object> ConnectAndQuery(string query){
         //Establishing connection and executing query against db
-        
         string activeDBPath = ChooseDBPath();
-        List<Object> messages = new List<Object>();
 
         using (var connection = new SqliteConnection($"Data Source = {activeDBPath}")){
             //Connecting to db, and executing query against it
@@ -36,24 +35,18 @@ public class DBFacade{
             command.CommandText = query;
             using var reader = command.ExecuteReader(); 
 
-            //Iterating through query results and adding them to a list
-            while (reader.Read()){
-                Object[] values = new Object[reader.FieldCount];
-                int fieldCount = reader.GetValues(values);
-                for (int i = 0; i < fieldCount; i++)
-                    messages.Add($"{reader.GetName(i)}: {values[i]}");
-            }
+            
+            return ReadQueryResult(reader);
 
         }
-        //return list of messages
-        return messages;
+       
 
     }
 
     //Method for retrieving all cheeps, in abstracted form
     public List<Object> GetAllMessages(){
         //Query that retrieves all messages
-        string query = @"SELECT * FROM message ORDER by message.pub_date desc";
+        string query = @"SELECT user.username, message.text, message.pub_date FROM message JOIN user on message.author_id = user.user_id ORDER by message.pub_date desc";
         return ConnectAndQuery(query);
     }
 
@@ -61,11 +54,8 @@ public class DBFacade{
     public List<Object> getAuthorsMessages(string author){
 
        //Only works if command gets parameterized
-        var query = @"SELECT * FROM message JOIN user ON message.author_id = user.user_id WHERE user.username = @author ORDER by message.pub_date desc";
-
+        var query = @"SELECT user.username, message.text, message.pub_date FROM message JOIN user ON message.author_id = user.user_id WHERE user.username = @author ORDER by message.pub_date desc";
         string activeDBPath = ChooseDBPath();
-        List<Object> messages = new List<Object>();
-
         using (var connection = new SqliteConnection($"Data Source = {activeDBPath}")){
             //Connecting to db, and executing query against it
             connection.Open();
@@ -76,14 +66,22 @@ public class DBFacade{
             using var reader = command.ExecuteReader(); 
 
             //Iterating through query results and adding them to a list
-            while (reader.Read()){
+            return ReadQueryResult(reader);
+
+        }
+        
+    }
+
+    private  List<Object> ReadQueryResult(SqliteDataReader reader){
+
+        List<Object> messages = new List<Object>();
+        while (reader.Read()){
                 Object[] values = new Object[reader.FieldCount];
                 int fieldCount = reader.GetValues(values);
                 for (int i = 0; i < fieldCount; i++)
-                    messages.Add($"{reader.GetName(i)}: {values[i]}");
-            }
-
-        }
+                    //messages.Add($"{reader.GetName(i)}: {values[i]}");
+                    messages.Add($"{values[i]}");
+         }
         return messages;
     }
     //Main is only there so I can test the fucntionality via dotnet run.
