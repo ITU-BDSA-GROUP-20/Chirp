e9bfa3832d656980a4fabbc6951ebee92d2fd27f
Oliver Laursen <olau@itu.dk>
2023-10-03 11:42:32 +0200

    Unittest for SQLite
    Co-Authored-By: Magnus Kristensen <bekr@itu.dk>
    Co-Authored-By: Mikkel Kristensen <mbek@itu.dk>
    Co-Authored-By: Mads Dalgaard Andersen <madsd@itu.dk>
    Co-Authored-By: Casper Holten <casho@itu.dk>

--- test/Chirp.SQLite.Tests/UnitTest.cs
+++ test/Chirp.SQLite.Tests/UnitTest.cs
@@ -12,6 +12,34 @@ public class UnitTest
         // Act
         List<Object> messages = db.GetAllMessages(1);
         // Assert
-        Assert.Equal(32, messages.Count);
+        //32 cheeps pr page amounts to 96 messages from the db. 
+        Assert.Equal(96, messages.Count);
+        //Messages are the building blocks of the cheeps ie author, content, timestamp
     }
+
+    [Theory]
+    [InlineData("Helge", "Hello, BDSA students!")]
+    [InlineData("Rasmus", "Hej, velkommen til kurset.")]
+    public void GetAuthorMessages_checkAuthorAndMessage(string author, string message){
+        //Arrange
+        DBFacade db = new();
+        //Act
+        List<Object> messages = db.getAuthorsMessages(author, 1);
+        //Assert
+        Assert.Equal(author, messages[0]);
+        Assert.Equal(message,messages[1]);
+    }
+
+    [Theory]
+    [InlineData("Helge", "Hello, BDSA students!")]
+    [InlineData("Rasmus", "Hej, velkommen til kurset.")]
+    public void GetAuthorsMessages_SecondPage(string author, string message){
+        //Arrange
+        DBFacade db = new();
+        //Act
+        List<Object> messages = db.getAuthorsMessages(author, 2);
+        //Assert
+        Assert.Equal(0, messages.Count());
+    }
+
 }
