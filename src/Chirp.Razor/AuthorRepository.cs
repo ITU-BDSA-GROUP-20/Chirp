a59213825f7da879bd6a3d8f102501d2c321feb6
Magnus Kristensen <bekr@itu.dk>
2023-10-10 11:59:55 +0200

    Implemented GetCheepsByAuthor method
    Enabled getting cheeps by author object - can be changed to be by author ID or author name.
    Currently guards in case author has no cheeps on record
    
    Co-Authored-By: Mikkel Kristensen <mbek@itu.dk>

--- src/Chirp.Razor/AuthorRepository.cs
+++ src/Chirp.Razor/AuthorRepository.cs
@@ -7,8 +7,17 @@ public class AuthorRepository : BaseRepository, IAuthorRepository
     
     public IEnumerable<CheepViewModel> GetCheepsByAuthor(Author author)
     {
-        //Get cheeps by Author object
+        //Check that author has cheeps
+        if (!db.Cheeps.Any(c => c.AuthorId == author.AuthorId))
+        {
+            throw new Exception("Author " + author.Name + " has no cheeps");
+        }
         
+        //Get cheeps by Author object
+        return db.Cheeps
+            .Where(c => c.AuthorId == author.AuthorId)
+            .Select(c => new CheepViewModel(author.Name, c.Message, c.Timestamp))
+            .ToList();
     }
     
     public String GetAuthorById(int authorId)
