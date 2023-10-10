68e219c27c060ee2db50119382d8eb82fe290a1e
Oliver Laursen <olau@itu.dk>
2023-10-10 10:45:37 +0200

    Bugfix
    Bugfixing on dbcontext
    
    Co-Authored-By: Mads Dalgaard Andersen <madsd@itu.dk>
    Co-Authored-By: Casper Holten <casho@itu.dk>

--- src/Chirp.Razor/ChirpContext.cs
+++ src/Chirp.Razor/ChirpContext.cs
@@ -1,10 +1,8 @@
 using Chirp.Razor.Models;
+using Microsoft.EntityFrameworkCore;
 //This file holds the datamodel consisting of Author, Cheep, and ChirpDbContext
 //EF Core will use the properties of these classes to create and control
 //the database, without having the application directly interact with the database. 
-
-
-
 
 public class ChirpDBContext : DbContext
 {
