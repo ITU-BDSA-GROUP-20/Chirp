465eb2491d7da1402c337c34581a2c0a0751db95
Mikkel Kristensen <mbek@itu.dk>
2023-10-10 12:40:46 +0200

    Attempt to get around merge conflicts
    Attempt to get around merge conflicts that github did not easily help resolve

--- src/Chirp.Razor/ChirpDbContext.cs
+++ src/Chirp.Razor/ChirpDbContext.cs
@@ -5,15 +5,39 @@ using Microsoft.EntityFrameworkCore;
 //EF Core will use the properties of these classes to create and control
 //the database, without having the application directly interact with the database. 
 
+namespace Chirp.Razor;
 
-
-
-public class ChirpDbContext : DbContext
+public class ChirpDBContext : DbContext
 {
-    public ChirpDbContext(DbContextOptions<ChirpDbContext> options) : base(options)
-    {
-    }
     public DbSet<Author> Authors {get; set;}
     public DbSet<Cheep> Cheeps {get; set;}
+    //Source for methods:
+    //https://www.c-sharpcorner.com/article/get-started-with-entity-framework-core-using-sqlite/
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=./data/ChirpDBContext.db");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(Entity =>
+        {
+            Entity.HasKey(e => e.AuthorId);
+            Entity.Property(e => e.Name).IsRequired();
+            Entity.Property(e => e.Email).IsRequired();
+            Entity.HasMany(e => e.Cheeps);
+        });
+
+        modelBuilder.Entity<Cheep>(Entity =>
+        {
+            Entity.HasKey(e => e.CheepId);
+            Entity.Property(e => e.Text).IsRequired();
+            Entity.Property(e => e.TimeStamp).IsRequired();
+            Entity.HasOne(e => e.Author)
+                .WithMany(author => author.Cheeps)
+                .HasForeignKey(e => e.AuthorId);
+        });
+    }
+
 
 }
