3dec8a0e5807bb2dbcb35eedfdc62a029ae3ccd6
Mikkel Kristensen <mbek@itu.dk>
2023-10-11 11:53:40 +0200

    Refactor AuthorRepo to make it compatible with other classes
    Co-Authored-By: Oliver Laursen <olau@itu.dk>

--- src/Chirp.Razor/Repository/AuthorRepository.cs
+++ src/Chirp.Razor/Repository/AuthorRepository.cs
@@ -4,8 +4,15 @@ namespace Chirp.Razor.Repository;
 
 public class AuthorRepository : BaseRepository, IAuthorRepository
 {
-    
-    public IEnumerable<CheepViewModel> GetCheepsByAuthor(Author author)
+    public String GetAuthorById(int authorId)
+    {
+        return db.Authors
+            .Where(a => a.AuthorId == authorId)
+            .Select(a => a.Name)
+            .FirstOrDefault()!;
+    }
+
+    public ICollection<Cheep> GetCheepsByAuthor(Author author)
     {
         //Check that author has cheeps
         if (!db.Cheeps.Any(c => c.AuthorId == author.AuthorId))
@@ -13,19 +20,9 @@ public class AuthorRepository : BaseRepository, IAuthorRepository
             throw new Exception("Author " + author.Name + " has no cheeps");
         }
         
-        //Get cheeps by Author object
-        //@TODO: Refactor to use db.Author as it holds a collection of Cheeps when returning. 
-        return db.Cheeps
-            .Where(c => c.AuthorId == author.AuthorId)
-            .Select(c => new CheepViewModel(author.Name, c.Text, c.TimeStamp.ToString()))
-            .ToList();
-    }
-    
-    public String GetAuthorById(int authorId)
-    {
+        // select Authors entire iCollection of Cheeps
         return db.Authors
-            .Where(a => a.AuthorId == authorId)
-            .Select(a => a.Name)
-            .FirstOrDefault()!;
+            .Where(a => a.AuthorId == author.AuthorId)
+            .Select(a => a.Cheeps).FirstOrDefault()!;
     }
 }
