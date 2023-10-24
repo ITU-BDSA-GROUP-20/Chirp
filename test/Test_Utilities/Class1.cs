a0c88834bc7ccff0cbf08d6e20e42dea5838901f
Magnus Kristensen <bekr@itu.dk>
2023-10-24 14:02:07 +0200

    Set up the skeleton for the ContextFactory
    This will be the bare necessity to create a context to be used in other test files.
    
    Co-Authored-By: Oliver Laursen <olau@itu.dk>

--- test/Test_Utilities/Class1.cs
+++ test/Test_Utilities/Class1.cs
@@ -1,5 +1,16 @@
 ﻿namespace Test_Utilities;
 
-public class 
+
+public class SqliteInMemoryChirpConnectionBuilder
 {
+    var _contextOptions = new DbContextOptionsBuilder<ChirpDbContext>()
+        .UseInMemoryDatabase(databaseName: "Chirp")
+        .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+        .Options;
+    using var context = new ChirpDbContext(_contextOptions);
+    
+    public ChirpDbContext GetContext()
+    {
+        return context;
+    }
 }
