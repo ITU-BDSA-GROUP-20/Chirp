80bd3c68ed691e9e04f5d2b350b464b70bf7bfe3
Magnus Kristensen <bekr@itu.dk>
2023-10-24 14:19:09 +0200

    Implemented simple methods to add authors and cheeps to context
    Database integrity is now ensured OnGet and on instantiation of builder - maybe redundant?
    Adding Cheep and Author objects is super easy, barely an inconvenience.
    
    Co-Authored-By: Oliver Laursen <olau@itu.dk>

--- test/Test_Utilities/Class1.cs
+++ test/Test_Utilities/Class1.cs
@@ -8,9 +8,28 @@ public class SqliteInMemoryChirpConnectionBuilder
         .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
         .Options;
     using var context = new ChirpDbContext(_contextOptions);
+
+	public SqliteInMemoryChirpConnectionBuilder(){
+		context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+    }
+    
+    public void AddAuthor(AuthorDTO author)
+    {
+        context.Authors.Add(author);
+        context.SaveChanges();
+    }
+    
+    public void AddCheep(CheepDTO cheep)
+    {
+        context.Cheeps.Add(cheep);
+        context.SaveChanges();
+	}
     
     public ChirpDbContext GetContext()
     {
+		context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
         return context;
     }
 }
