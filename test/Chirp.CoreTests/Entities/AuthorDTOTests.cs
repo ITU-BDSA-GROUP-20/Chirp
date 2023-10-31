38e15796b3cc3276fff15dbdcd57af9f90a0d3a6
Mikkel <mbek@itu.dk>
2023-10-31 13:41:46 +0100

    Create AuthorDTO tests that pass
    Co-Authored-By: Magnus Kristensen <bekr@itu.dk>

--- test/Chirp.CoreTests/Entities/AuthorDTOTests.cs
+++ test/Chirp.CoreTests/Entities/AuthorDTOTests.cs
@@ -1,41 +1,71 @@
-﻿using Chirp.Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Chirp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
-//Co-Authored by ChatGPT-4.0
+//Co-Authored by ChatGPT-3.5
 
 namespace Chirp.CoreTests.Entities;
 
 public class AuthorDTOTests
 {
     [Fact]
-    public void AuthorDTO_DefaultConstructor_SetsPropertiesToDefault()
+    public void AuthorDTO_Name_ShouldHaveRequiredAttribute()
     {
-        // Arrange & Act
-        var author = new AuthorDTO();
+        var propertyInfo = typeof(AuthorDTO).GetProperty("Name");
+        var attribute = propertyInfo.GetCustomAttribute<RequiredAttribute>();
+        Assert.NotNull(attribute);
+    }
+    
+    [Fact]
+    public void AuthorDTO_Name_ShouldHaveStringLengthAttributeWithMax50()
+    {
+        var propertyInfo = typeof(AuthorDTO).GetProperty("Name");
+        var stringLengthAttribute = propertyInfo.GetCustomAttributes(typeof(StringLengthAttribute), true).FirstOrDefault() as StringLengthAttribute;
 
-        // Assert
-        Assert.Equal(0, author.AuthorId);
-        Assert.Null(author.Name);
-        Assert.Null(author.Email);
-        Assert.Null(author.Cheeps);
+        Assert.NotNull(stringLengthAttribute);
+        Assert.Equal(50, stringLengthAttribute.MaximumLength);
+    }
+    
+    [Fact]
+    public void AuthorDTO_Email_ShouldHaveRequiredAttribute()
+    {
+        var propertyInfo = typeof(AuthorDTO).GetProperty("Email");
+        var requiredAttribute = propertyInfo.GetCustomAttributes(typeof(RequiredAttribute), true).FirstOrDefault() as RequiredAttribute;
+
+        Assert.NotNull(requiredAttribute);
     }
 
     [Fact]
-    public void AuthorDTO_SetProperties_PropertiesAreSet()
+    public void AuthorDTO_Email_ShouldHaveStringLengthAttributeWithMax50()
     {
-        // Arrange
-        var author = new AuthorDTO();
-        var cheeps = new List<CheepDTO>();
+        var propertyInfo = typeof(AuthorDTO).GetProperty("Email");
+        var stringLengthAttribute = propertyInfo.GetCustomAttributes(typeof(StringLengthAttribute), true).FirstOrDefault() as StringLengthAttribute;
 
-        // Act
-        author.AuthorId = 1;
-        author.Name = "Test Author";
-        author.Email = "test@email.com";
-        author.Cheeps = cheeps;
+        Assert.NotNull(stringLengthAttribute);
+        Assert.Equal(50, stringLengthAttribute.MaximumLength);
+    }
 
-        // Assert
-        Assert.Equal(1, author.AuthorId);
-        Assert.Equal("Test Author", author.Name);
-        Assert.Equal("test@email.com", author.Email);
-        Assert.Equal(cheeps, author.Cheeps);
+    [Fact]
+    public void AuthorDTO_IndexAttribute_ShouldBeUnique()
+    {
+        var indexAttribute = typeof(AuthorDTO).GetCustomAttributes(typeof(IndexAttribute), true).FirstOrDefault() as IndexAttribute;
+
+        Assert.NotNull(indexAttribute);
+        Assert.True(indexAttribute.IsUnique);
+    }
+
+    [Fact]
+    public void AuthorDTO_Cheeps_ShouldBeInitialized()
+    {
+        AuthorDTO author = new AuthorDTO
+        { 
+            AuthorId = Guid.NewGuid(), 
+            Name = "TestAuthor", 
+            Email = "mock@email.com" 
+        };
+
+        Assert.NotNull(author.Cheeps);
+        Assert.Empty(author.Cheeps);
     }
 }
