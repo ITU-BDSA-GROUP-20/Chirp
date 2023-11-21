de827093dcd885bdddbab56276f6a59a88d1738e
Mikkel Kristensen <mbek@itu.dk>
2023-11-21 12:29:34 +0100

    Add method to interface

--- src/Chirp.Core/Repository/ICheepRepository.cs
+++ src/Chirp.Core/Repository/ICheepRepository.cs
@@ -6,5 +6,6 @@ public interface ICheepRepository
 {
     public ICollection<Cheep> GetCheepsByPage(int page);
     public Task AddCheep(CreateCheep cheepDto);
+    public Cheep CreateCheep2Cheep(CreateCheep cheep);
 
 }
