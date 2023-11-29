8900c23a6a709185c707e5750b642a077439aebb
Casper Holten <casho@itu.dk>
2023-11-29 10:27:43 +0100

    Cheep Validator Tests

--- src/Chirp.Infrastructure/Repository/CheepCreateValidator.cs
+++ src/Chirp.Infrastructure/Repository/CheepCreateValidator.cs
@@ -10,5 +10,4 @@ public class CheepCreateValidator : AbstractValidator<CreateCheep>
         // @TODO Check that these values are correct: 
         RuleFor(x => x.Text).NotEmpty().MaximumLength(160).MinimumLength(5).WithMessage("The Cheep must be between 5 and 160 characters.(CheepCreateValidator)");
     }
-    
 }
