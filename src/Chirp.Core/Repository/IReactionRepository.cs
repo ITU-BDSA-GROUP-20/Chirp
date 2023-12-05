5d23d881f26bff4ecf98bbdf0797a2f3e1288522
olau <olau@itu.dk>
2023-12-05 22:39:28 +0100

    ReactionRepository update
    Discovered a flaw in the way we planned to render the reactions, so I've decided to create a GetReactionCount, which can be called upon each reaction on a cheep.

--- src/Chirp.Core/Repository/IReactionRepository.cs
+++ src/Chirp.Core/Repository/IReactionRepository.cs
@@ -6,4 +6,6 @@ public interface IReactionRepository
 {
     public  Task AddReaction(ReactionType reaction, Guid cheepId, Guid authorId);
     public Task RemoveReaction(ReactionType reaction, Guid cheepId, Guid authorId);
+    
+    public Task<int> GetReactionCount(Guid cheepId, ReactionType reactionType);
 }
--- src/Chirp.Infrastructure/Repository/ReactionRepository.cs
+++ src/Chirp.Infrastructure/Repository/ReactionRepository.cs
@@ -1,5 +1,6 @@
 using Chirp.Core.Entities;
 using Chirp.Core.Repository;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chirp.Infrastructure.Repository;
 
@@ -34,18 +35,25 @@ public class ReactionRepository: BaseRepository, IReactionRepository
     }
     public async Task RemoveReaction(ReactionType reaction, Guid cheepId, Guid authorId)
     {
-        try
-        {
             Reaction? entity = await db.Reactions.FindAsync(cheepId, reaction, authorId);
             if (entity != null) db.Reactions.Remove(entity);
             await db.SaveChangesAsync();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("ReactionRepository: "+ e);
-            throw;
-        }
-       
         
     }
+    public async Task<int> GetReactionCount(Guid cheepId, ReactionType reactionType)
+    {
+           Cheep? cheep = await db.Cheeps.FindAsync(cheepId);
+           int count = 0; 
+            if (cheep != null)
+           { 
+               count = await db.Reactions.CountAsync(r => r.Cheep == cheep && r.ReactionType == reactionType);
+           }
+           else
+           {
+               throw new Exception("Cheep with id " + cheepId + " not found");
+           }
+
+            return count;
+    }
 }
+
