3b3eed83875af73b2cc93f789741ea5898eea868
Mikkel Kristensen <mbek@itu.dk>
2023-12-06 13:21:20 +0100

    Add the follower collections to the usermodel to display it on aboutMe page

--- src/Chirp.Web/Models/Usermodel.cs
+++ src/Chirp.Web/Models/Usermodel.cs
@@ -1,17 +1,28 @@
-﻿namespace Chirp.Web.Models;
+﻿using Chirp.Infrastructure;
+using Chirp.Infrastructure.Repository;
+
+namespace Chirp.Web.Models;
 
 using Chirp.Core.Entities;
+using Chirp.Core.Repository;
 
 public class UserModel
 {
+    public Guid Id { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
+    public ICollection<Author> Followers { get; set; }
+    public ICollection<Author> Following { get; set; }
+    
 
     // Constructor to initialize properties based on an Author entity
     public UserModel(Author author)
     {
+        Id = author.Id;
         Username = author.UserName;
         Email = author.Email;
+        Followers = author.Followers;
+        Following = author.Following;
     }
 }
 
