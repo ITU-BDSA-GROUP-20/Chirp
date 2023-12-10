d32b826dceb06bbaf04250ef8434d72ec41a826e
Casper Holten <casho@itu.dk>
2023-12-10 22:39:55 +0100

    Revert "Revert "Revert "Merge pull request #187 from ITU-BDSA23-GROUP20/BasePageModel"""
    This reverts commit db9f7db3319591d8d074303548ff9ce469ba4dbb.

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,33 +1,69 @@
 using Chirp.Core.Entities;
 using Chirp.Core.Repository;
-using FluentValidation;
+using Chirp.Web;
+using Chirp.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Chirp.Web.Pages;
+namespace Chirp.Razor.Pages;
 
-public class UserTimelineModel : TimeLineModel
+public class UserTimelineModel : PageModel
 {
-    public UserTimelineModel(ICheepService cheepService,
-        ICheepRepository cheepRepository,
-        IAuthorRepository authorRepository,
-        IReactionRepository reactionRepository,
-        IValidator<CreateCheep> cheepValidator,
-        UserManager<Author> userManager) : base(
-        cheepService,
-        cheepRepository,
-        authorRepository,
-        reactionRepository,
-        cheepValidator,
-        userManager
-    )
+    private readonly ICheepService _service;
+    private readonly UserManager<Author> _userManager;
+    private readonly IAuthorRepository _authorRepository;
+
+
+    public ICollection<CheepViewModel> Cheeps { get; set; }
+    public UserModel UserModel { get; set; }
+    
+    public required Author user { get; set; }
+    public required int totalPages { get; set; }
+
+
+
+    public UserTimelineModel(ICheepService service, UserManager<Author> userManager, IAuthorRepository authorRepository)
     {
-        
+        _service = service;
+        _userManager = userManager;
+        _authorRepository = authorRepository;
     }
 
-    
-    protected override void InitializeCheepList()
+    public ActionResult OnGet(string author)
     {
-        Author timelineAuthor = _authorRepository.GetAuthorByName(PageContext.RouteData.Values["author"].ToString());
-        Cheeps = _cheepService.GetCheepsFromAuthor(timelineAuthor.Id, CurrentPage);
+        user = _userManager.GetUserAsync(User).Result;
+        if (user == null)
+        {
+            return NotFound();
+        }
+        
+        UserModel = new UserModel(user);
+        
+        int page;
+        if(Request.Query.ContainsKey("page")){
+            page = int.Parse(Request.Query["page"]);
+        } else{
+            page = 1;
+        }
+        
+        try
+        {
+            Cheeps = _service.GetCheepsFromAuthor(UserModel.Id, page);
+        }
+        catch (Exception e)
+        {
+            Cheeps = new List<CheepViewModel>();
+        }
+
+        user = _userManager.GetUserAsync(User).Result;
+        
+        //Get author object to allow the get page count method to be called on ID
+        Author authorObject = _authorRepository.GetAuthorByName(author);
+        
+        totalPages = _authorRepository.GetPageCountByAuthor(authorObject.Id);
+
+        return Page();
     }
 }
