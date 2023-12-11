f99d8e86013ad7ce57c2c9b91bd5bb70fe03402d
Mikkel Kristensen <mbek@itu.dk>
2023-12-11 16:53:23 +0100

    Create intial test

---
+++ test/Chirp.InfrastructureTests/RepositoryTests/FollowRepositoryTest.cs
@@ -0,0 +1,48 @@
+﻿using Chirp.Core.Entities;
+using Chirp.Infrastructure;
+using Chirp.Infrastructure.Repository;
+using Test_Utilities;
+
+namespace Chirp.InfrastructureTest.RepositoryTests;
+
+public class FollowRepositoryTest
+{
+    private readonly ChirpDbContext context;
+
+    public FollowRepositoryTest()
+    {
+        context = SqliteInMemoryBuilder.GetContext();
+    }
+
+    [Fact]
+    public void CreateFollow_ShouldCreateFollow()
+    {
+        // Arrange
+        FollowRepository followRepository = new(context);
+        
+        Author follower = new Author()
+        {
+            Id = Guid.NewGuid(),
+            UserName = "Follower",
+            Email = "Follower@mail.com",
+        };
+        Author following = new Author()
+        {
+            Id = Guid.NewGuid(),
+            UserName = "Following",
+            Email = "Following@mail.com"
+        };
+        
+        Follow manuelFollow = new()
+        {
+            Follower = follower,
+            Following = following
+        };
+        
+        // Act
+        Follow generatedFollow = followRepository.CreateFollow(follower, following);
+        
+        // Assert
+        Assert.Equal(generatedFollow, manuelFollow);
+    }
+}
