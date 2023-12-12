30a526f56652b9c48834baee3633ace1f37c10ff
Mikkel Kristensen <mbek@itu.dk>
2023-12-12 12:06:54 +0100

    Now has two collections again

--- src/Chirp.Core/Entities/Author.cs
+++ src/Chirp.Core/Entities/Author.cs
@@ -21,5 +21,6 @@ public class Author : IdentityUser<Guid> {
     
     public ICollection<Reaction>? Reactions { get; set; }
     
-    public ICollection<Follow> Follows { get; set; } = new List<Follow>();
+    public ICollection<Follow> Followers { get; set; } = new List<Follow>();
+    public ICollection<Follow> Following { get; set; } = new List<Follow>();
 }
