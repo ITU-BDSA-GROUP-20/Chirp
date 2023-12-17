ea60253f2c4b8e915b57c91935f465f3613b8da9
olau <olau@itu.dk>
2023-12-17 10:37:51 +0100

    Update summary

--- src/Chirp.Core/Entities/Reaction.cs
+++ src/Chirp.Core/Entities/Reaction.cs
@@ -1,6 +1,8 @@
 ﻿namespace Chirp.Core.Entities;
 /// <summary>
 /// This class represents the reaction a User can give to a cheep.
+/// The reactions can be either a Like, Dislike, or Love.
+/// Defined in ReactionType.cs.
 /// </summary>
 public class Reaction {
     public Guid CheepId { get; set; }
