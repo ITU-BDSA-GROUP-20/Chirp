e696050acf5113dc64d51db3ab7a3e052d67c8dd
Casper Holten <casho@itu.dk>
2023-12-18 12:07:22 +0100

    Refactored CheepService tests to be more extensible
    Moved entity creation to test constructor, to minimise repetitive code.

--- test/Chirp.WebTests/CheepServiceUnitTests.cs
+++ test/Chirp.WebTests/CheepServiceUnitTests.cs
@@ -11,102 +11,92 @@ namespace Chirp.WebTests;
 
 public class CheepServiceUnitTests
 {
-    private readonly ChirpDbContext context;
-    private ICheepRepository _cheepRepository;
-    private IAuthorRepository _authorRepository;
-    private IReactionRepository _reactionRepository;
+    private CheepService _service;
+
+    private Author _author1;
+    private Author _author2;
+    private Cheep _cheep1;
+    private Cheep _cheep2;
+    private Cheep _cheep3;
 
     public CheepServiceUnitTests()
     {
-        context = SqliteInMemoryBuilder.GetContext();
-    }
-    
-    [Fact]
-    public void GetCheeps_ReturnsCheepViewModels()
-    {
-        // Arrange
-        _cheepRepository = new CheepRepository(context);
-        _authorRepository = new AuthorRepository(context);
-        _reactionRepository = new ReactionRepository(context);
-        ICheepService service = new CheepService(_cheepRepository, _authorRepository, _reactionRepository);
-        
-        // Mock data
-        Author author1 = new Author { Id = Guid.NewGuid(), UserName = "Author1", Email = "email1" };
-        Author author2 = new Author { Id = Guid.NewGuid(), UserName = "Author2", Email = "email2" };
-        
-        CreateCheep cheep1 = new CreateCheep(author1, "Cheep 1");
-        CreateCheep cheep2 = new CreateCheep(author2, "Cheep 2");
+        ChirpDbContext context = SqliteInMemoryBuilder.GetContext();
+        IFollowRepository followRepository = new FollowRepository(context);
+        ICheepRepository cheepRepository = new CheepRepository(context);
+        IAuthorRepository authorRepository = new AuthorRepository(context);
+        IReactionRepository reactionRepository = new ReactionRepository(context);
+        _service = new CheepService(cheepRepository, authorRepository, reactionRepository);
 
-        List<CreateCheep> cheepDtos = new List<CreateCheep>();
-        cheepDtos.Add(cheep1);
-        cheepDtos.Add(cheep2);
-        
-        // Add authors to database
-        _authorRepository.AddAuthor(author1);
-        _authorRepository.AddAuthor(author2);
-        
-        // Add cheeps to database
-        foreach (CreateCheep cheep in cheepDtos)
+        _author1 = new Author { Id = Guid.NewGuid(), UserName = "Author1", Email = "email1" };
+        _author2 = new Author { Id = Guid.NewGuid(), UserName = "Author2", Email = "email2" };
+
+        _cheep1 = new Cheep
         {
-            _cheepRepository.AddCreateCheep(cheep);
-        }
-
-        // Act
-        List<CheepViewModel> result = service.GetCheeps(0).ToList();
-
-        result.Sort((a, b) => String.Compare(a.User.Username, b.User.Username, StringComparison.Ordinal));
-            
-        // Assert
-        Assert.Equal(2, result.Count);
-        Assert.Equal("Author1", result[0].User.Username);
-        Assert.Equal("Cheep 1", result[0].Message);
-        Assert.NotNull(result[0].Timestamp);
-    }
-    
-    
-    [Fact]
-    public void GetCheepsFromAuthor_ReturnsCheepViewModels()
-    {
-        // Arrange
-        var cheepRepositoryMock = new Mock<ICheepRepository>();
-        var authorRepositoryMock = new Mock<IAuthorRepository>();
-        var reactionRepositoryMock = new Mock<IReactionRepository>();
-        var cheepService = new CheepService(cheepRepositoryMock.Object, authorRepositoryMock.Object, reactionRepositoryMock.Object );
+            Author = _author1,
+            AuthorId = _author1.Id,
+            Text = "Cheep 1",
+            TimeStamp = DateTime.Now,
+            Reactions = new List<Reaction>(),
+        };
         
-        var author1 = new Author { Id = Guid.NewGuid(), UserName = "Author1", Email = "email1" };
-        var author2 = new Author { Id = Guid.NewGuid(), UserName = "Author2", Email = "email2" };
-        
-        var cheeps = new List<Cheep>
+        _cheep2 = new Cheep
         {
-            new Cheep
-            {
-                Author = author1,
-                AuthorId = author1.Id,
-                Text = "Cheep 1",
-                TimeStamp = DateTime.Now,
-                Reactions = new List<Reaction>()
-            },
-            new Cheep
-            {
-                Author = author2,
-                AuthorId = author2.Id,
-                Text = "Cheep 2",
-                TimeStamp = DateTime.Now,
-                Reactions = new List<Reaction>()
-            }
+            Author = _author2,
+            AuthorId = _author2.Id,
+            Text = "Cheep 2",
+            TimeStamp = DateTime.Now,
+            Reactions = new List<Reaction>()
         };
 
-        authorRepositoryMock.Setup(repo => repo.GetCheepsByAuthor(author1.Id, It.IsAny<int>())).Returns(cheeps);
+        _cheep3 = new Cheep()
+        {
+            Author = _author2,
+            AuthorId = _author2.Id,
+            Text = "Cheep 3",
+            TimeStamp = DateTime.Now,
+            Reactions = new List<Reaction>()
+        };
 
-        // Act
-        ICollection<CheepViewModel> result = cheepService.GetCheepsFromAuthor(author1.Id, 1);
+        followRepository.CreateFollow(_author1, _author2);
 
-        CheepViewModel returnedCheep = result.ElementAt(0);
-        
-        // Assert
-        Assert.Equal(2, result.Count);
-        Assert.Equal(author1.UserName, returnedCheep.User.Username);
-        Assert.Equal("Cheep 1", returnedCheep.Message);
-        Assert.NotNull(returnedCheep.Timestamp);
+        context.Add(_author1);
+        context.Add(_author2);
+        context.Add(_cheep1);
+        context.Add(_cheep2);
+        context.Add(_cheep3);
+
+        context.SaveChanges();
     }
-}
+
+    [Fact]
+        public void GetCheeps_ReturnsCheepViewModels()
+        {
+            // Act
+            List<CheepViewModel> result = _service.GetCheeps(0).ToList();
+
+            result.Sort((a, b) => String.Compare(a.User.Username, b.User.Username, StringComparison.Ordinal));
+            
+            // Assert
+            Assert.Equal(2, result.Count);
+            Assert.Equal("Author1", result[0].User.Username);
+            Assert.Equal("Cheep 1", result[0].Message);
+            Assert.NotNull(result[0].Timestamp);
+        }
+    
+    
+        [Fact]
+        public void GetCheepsFromAuthor_ReturnsCheepViewModels()
+        {
+            // Act
+            ICollection<CheepViewModel> result = _service.GetCheepsFromAuthor(_author2.Id, 1);
+
+            CheepViewModel returnedCheep = result.ElementAt(0);
+        
+            // Assert
+            Assert.Equal(2, result.Count);
+            Assert.Equal(_author2.UserName, returnedCheep.User.Username);
+            Assert.Equal("Cheep 3", returnedCheep.Message);
+            Assert.NotNull(returnedCheep.Timestamp);
+        }
+    }
