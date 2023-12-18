117d8354faf36764c2b0a04dcdae02a38e1b3c69
Mikkel Kristensen <mbek@itu.dk>
2023-12-18 10:37:39 +0100

    Add migrations
    Co-Authored-By: Oliver Laursen <olau@itu.dk>

---
+++ src/Chirp.Infrastructure/Migrations/20231218092325_FixLoginsMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixLoginsMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLogins_LoginProvider_ProviderKey",
+                table: "UserLogins",
+                columns: new[] { "LoginProvider", "ProviderKey" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserLogins_LoginProvider_ProviderKey",
+                table: "UserLogins");
+        }
+    }
+}
